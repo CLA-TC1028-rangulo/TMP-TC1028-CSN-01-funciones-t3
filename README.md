d71b3668591dd45c4ef51ccce5e0dd8eeb1d202d
pangulotecmx <88519161+pangulotecmx@users.noreply.github.com>
2022-08-27 19:42:54 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+![Tec de Monterrey](images/logotecmty.png)
+# Ejercicios con funciones
+
+- 02 Calcula Grado
+
---
+++ assignments/02CalculaGrado/src/exercise.py
@@ -0,0 +1,7 @@
+def main():
+    #escribe tu código abajo de esta línea
+    x = float(input("Ingresa Un valor entre 0.0 y 1.0: "))
+    print(calcula_grado(x))
+
+if __name__=='__main__':
+    main()
